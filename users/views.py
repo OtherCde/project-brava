239472fcb8abed40a251ef58e86d010e2dd14b03
Agustin <juanarquipa6675@gmail.com>
2025-03-11 20:58:56 -0300

    Correcion de view de GOOGLE para poder iniciar session

--- users/views.py
+++ users/views.py
@@ -430,7 +430,7 @@ class GoogleLogin(APIView):
                 "code": code,
                 "client_id": settings.GOOGLE_CLIENT_ID,
                 "client_secret": settings.GOOGLE_CLIENT_SECRET,
-                "redirect_uri": "http://localhost:5173",  # Asegúrate de que coincide con Google Console
+                "redirect_uri": "https://agu.dev.demo.okarol.com/users/login/google/callback/",  # Asegúrate de que coincide con Google Console
                 "grant_type": "authorization_code",
             }
 

aa173d2a2eabe10576bf4b3a42b3c6e7dfc9cea6
Agustin <juanarquipa6675@gmail.com>
2025-03-20 16:06:56 -0300

    Modificacion de view Publicidades para obtener todas sin necesidad de un token de autenticacion

--- adverstisements/views.py
+++ adverstisements/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Advertisement
 from .serializers import AdvertisementSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
 class AdvertisementListView(generics.ListAPIView):
     serializer_class = AdvertisementSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
     def get_queryset(self):
         """Retorna solo las publicidades activas."""
